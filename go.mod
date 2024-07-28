cdc50339cdcca6aaf4f26f8bd2c509b2da3d5ada
popeguy <kevinpope.co@gmail.com>
2024-07-28 19:49:54 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,40 @@
+module main //github.com/pion/example-webrtc-applications/v3
+
+go 1.22
+
+toolchain go1.22.4
+
+require (
+	github.com/at-wat/ebml-go v0.17.1
+	github.com/gorilla/websocket v1.4.2
+	github.com/pion/rtcp v1.2.14
+	github.com/pion/rtp v1.8.6
+	github.com/pion/webrtc/v3 v3.2.40
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pion/datachannel v1.5.5 // indirect
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/ice/v2 v2.3.24 // indirect
+	github.com/pion/interceptor v0.1.25 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/mdns v0.0.12 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/sctp v1.8.16 // indirect
+	github.com/pion/sdp/v3 v3.0.9 // indirect
+	github.com/pion/srtp/v2 v2.0.18 // indirect
+	github.com/pion/stun v0.6.1 // indirect
+	github.com/pion/transport/v2 v2.2.4 // indirect
+	github.com/pion/turn/v2 v2.1.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,310 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+//go:build !js
+// +build !js
+
+// save-to-webm is a simple application that shows how to receive audio and video using Pion and then save to WebM container.
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/at-wat/ebml-go/webm"
+	"github.com/gorilla/websocket"
+	"github.com/pion/rtcp"
+	"github.com/pion/rtp"
+	"github.com/pion/rtp/codecs"
+	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+
+	saver := newWebmSaver()
+
+	peerConnection := createWebRTCConn(saver)
+
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		for {
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			rmsg := strings.TrimSpace(string(msg[:]))
+			offer := webrtc.SessionDescription{}
+			decode(rmsg, &offer)
+
+			err = peerConnection.SetRemoteDescription(offer)
+			if err != nil {
+				panic(err)
+			}
+
+			// Create answer
+			answer, err := peerConnection.CreateAnswer(nil)
+			if err != nil {
+				panic(err)
+			}
+
+			// Create channel that is blocked until ICE Gathering is complete
+			gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
+			// Sets the LocalDescription, and starts our UDP listeners
+			err = peerConnection.SetLocalDescription(answer)
+			if err != nil {
+				panic(err)
+			}
+
+			// Block until ICE Gathering is complete, disabling trickle ICE
+			// we do this because we only can exchange one signaling message
+			// in a production application you should exchange ICE Candidates via OnICECandidate
+			<-gatherComplete
+
+			// Output the answer in base64 so we can paste it in browser
+			//fmt.Println(encode(peerConnection.LocalDescription()))
+			if err = conn.WriteMessage(websocket.TextMessage, []byte(encode(peerConnection.LocalDescription()))); err != nil {
+				return
+			}
+
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	http.ListenAndServe(":8080", nil)
+
+	//closed := make(chan os.Signal, 1)
+	//signal.Notify(closed, os.Interrupt)
+	//<-closed
+
+	//	if err := peerConnection.Close(); err != nil {
+	//	panic(err)
+	//}
+	saver.Close()
+}
+
+type webmSaver struct {
+	audioWriter, videoWriter       webm.BlockWriteCloser
+	audioBuilder, videoBuilder     *samplebuilder.SampleBuilder
+	audioTimestamp, videoTimestamp time.Duration
+}
+
+func newWebmSaver() *webmSaver {
+	return &webmSaver{
+		audioBuilder: samplebuilder.New(10, &codecs.OpusPacket{}, 48000),
+		videoBuilder: samplebuilder.New(10, &codecs.VP8Packet{}, 90000),
+	}
+}
+
+func (s *webmSaver) Close() {
+	fmt.Printf("Finalizing webm...\n")
+	if s.audioWriter != nil {
+		if err := s.audioWriter.Close(); err != nil {
+			panic(err)
+		}
+	}
+	if s.videoWriter != nil {
+		if err := s.videoWriter.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (s *webmSaver) PushOpus(rtpPacket *rtp.Packet) {
+	s.audioBuilder.Push(rtpPacket)
+
+	for {
+		sample := s.audioBuilder.Pop()
+		if sample == nil {
+			return
+		}
+		if s.audioWriter != nil {
+			s.audioTimestamp += sample.Duration
+			if _, err := s.audioWriter.Write(true, int64(s.audioTimestamp/time.Millisecond), sample.Data); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func (s *webmSaver) PushVP8(rtpPacket *rtp.Packet) {
+	s.videoBuilder.Push(rtpPacket)
+
+	for {
+		sample := s.videoBuilder.Pop()
+		if sample == nil {
+			return
+		}
+		// Read VP8 header.
+		videoKeyframe := (sample.Data[0]&0x1 == 0)
+		if videoKeyframe {
+			// Keyframe has frame information.
+			raw := uint(sample.Data[6]) | uint(sample.Data[7])<<8 | uint(sample.Data[8])<<16 | uint(sample.Data[9])<<24
+			width := int(raw & 0x3FFF)
+			height := int((raw >> 16) & 0x3FFF)
+
+			if s.videoWriter == nil || s.audioWriter == nil {
+				// Initialize WebM saver using received frame size.
+				s.InitWriter(width, height)
+			}
+		}
+		if s.videoWriter != nil {
+			s.videoTimestamp += sample.Duration
+			if _, err := s.videoWriter.Write(videoKeyframe, int64(s.videoTimestamp/time.Millisecond), sample.Data); err != nil {
+				panic(err)
+			}
+		}
+	}
+}
+
+func (s *webmSaver) InitWriter(width, height int) {
+	w, err := os.OpenFile("test.webm", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o600)
+	if err != nil {
+		panic(err)
+	}
+
+	ws, err := webm.NewSimpleBlockWriter(w,
+		[]webm.TrackEntry{
+			{
+				Name:            "Audio",
+				TrackNumber:     1,
+				TrackUID:        12345,
+				CodecID:         "A_OPUS",
+				TrackType:       2,
+				DefaultDuration: 20000000,
+				Audio: &webm.Audio{
+					SamplingFrequency: 48000.0,
+					Channels:          2,
+				},
+			}, {
+				Name:            "Video",
+				TrackNumber:     2,
+				TrackUID:        67890,
+				CodecID:         "V_VP8",
+				TrackType:       1,
+				DefaultDuration: 33333333,
+				Video: &webm.Video{
+					PixelWidth:  uint64(width),
+					PixelHeight: uint64(height),
+				},
+			},
+		})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("WebM saver has started with video width=%d, height=%d\n", width, height)
+	s.audioWriter = ws[0]
+	s.videoWriter = ws[1]
+}
+
+func createWebRTCConn(saver *webmSaver) *webrtc.PeerConnection {
+	// Everything below is the Pion WebRTC API! Thanks for using it ❤️.
+
+	// Prepare the configuration
+	config := webrtc.Configuration{}
+
+	// Create a MediaEngine object to configure the supported codec
+	m := &webrtc.MediaEngine{}
+
+	// Setup the codecs you want to use.
+	// Only support VP8 and OPUS, this makes our WebM muxer code simpler
+	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "video/VP8", ClockRate: 90000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
+		PayloadType:        96,
+	}, webrtc.RTPCodecTypeVideo); err != nil {
+		panic(err)
+	}
+	if err := m.RegisterCodec(webrtc.RTPCodecParameters{
+		RTPCodecCapability: webrtc.RTPCodecCapability{MimeType: "audio/opus", ClockRate: 48000, Channels: 0, SDPFmtpLine: "", RTCPFeedback: nil},
+		PayloadType:        111,
+	}, webrtc.RTPCodecTypeAudio); err != nil {
+		panic(err)
+	}
+
+	// Create the API object with the MediaEngine
+	api := webrtc.NewAPI(webrtc.WithMediaEngine(m))
+
+	// Create a new RTCPeerConnection
+	peerConnection, err := api.NewPeerConnection(config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Set a handler for when a new remote track starts, this handler copies inbound RTP packets,
+	// replaces the SSRC and sends them back
+	peerConnection.OnTrack(func(track *webrtc.TrackRemote, _ *webrtc.RTPReceiver) {
+		// Send a PLI on an interval so that the publisher is pushing a keyframe every rtcpPLIInterval
+		go func() {
+			ticker := time.NewTicker(time.Second * 3)
+			for range ticker.C {
+				errSend := peerConnection.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: uint32(track.SSRC())}})
+				if errSend != nil {
+					fmt.Println(errSend)
+				}
+			}
+		}()
+
+		fmt.Printf("Track has started, of type %d: %s \n", track.PayloadType(), track.Codec().RTPCodecCapability.MimeType)
+		for {
+			// Read RTP packets being sent to Pion
+			rtp, _, readErr := track.ReadRTP()
+			if readErr != nil {
+				if errors.Is(readErr, io.EOF) {
+					return
+				}
+				panic(readErr)
+			}
+			switch track.Kind() {
+			case webrtc.RTPCodecTypeAudio:
+				saver.PushOpus(rtp)
+			case webrtc.RTPCodecTypeVideo:
+				saver.PushVP8(rtp)
+			}
+		}
+	})
+	// Set the handler for ICE connection state
+	// This will notify you when the peer has connected/disconnected
+	peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+
+	return peerConnection
+}
+
+func encode(obj *webrtc.SessionDescription) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// Decode a base64 and unmarshal JSON into a SessionDescription
+func decode(in string, obj *webrtc.SessionDescription) {
+	b, err := base64.StdEncoding.DecodeString(in)
+	if err != nil {
+		panic(err)
+	}
+
+	if err = json.Unmarshal(b, obj); err != nil {
+		panic(err)
+	}
+}
